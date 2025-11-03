34400e08fd709a7c61fbca0ef901819b87cece93
Sarah Ella Stoltze <sstoltze@gmail.com>
2025-11-03 15:29:15 +0100

    Nix setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forEachSystem = (
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-darwin"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            f {
+              inherit system;
+              pkgs = import nixpkgs { inherit system; };
+            }
+          )
+      );
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShell { packages = with pkgs; [ nodejs ]; };
+        }
+      );
+    };
+}
